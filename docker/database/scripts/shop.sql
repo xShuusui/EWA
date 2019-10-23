212bd89acf762408e622fd8a1ba3beffbd946d5d
Julian Segeth <julian.segeth@stud.h-da.de>
2019-10-23 20:09:14 +0200

    Created database Dockerfile and changed the locale settings for de_DE.UTF-8 UTF-8 in database container.

--- docker/database/scripts/shop.sql
+++ docker/database/scripts/shop.sql
@@ -1,3 +1,5 @@
+ALTER DATABASE shop COLLATE = 'utf8_general_ci';
+
 CREATE TABLE `menu` (
 	`pizzaID` INT NOT NULL AUTO_INCREMENT,
 	`pizzaName` VARCHAR(40) NOT NULL,
@@ -8,7 +10,7 @@ CREATE TABLE `menu` (
 
 CREATE TABLE `order` (
 	`orderID` INT NOT NULL AUTO_INCREMENT,
-	`address` VARCHAR(60) NOT NULL,
+	`address` VARCHAR(60) COLLATE utf8_unicode_ci NOT NULL,
 	`orderTime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (`orderID`)
 );
@@ -17,7 +19,7 @@ CREATE TABLE `orderedPizza` (
 	`orderedPizzaID` INT NOT NULL AUTO_INCREMENT,
 	`orderID` INT NOT NULL,
 	`pizzaID` INT NOT NULL,
-	`status` varchar(40) NOT NULL,
+	`status` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
 	PRIMARY KEY (`orderedPizzaID`),
 	CONSTRAINT `fk_orderID` FOREIGN KEY (`orderID`) REFERENCES `order` (`orderID`),
 	CONSTRAINT `fk_pizzaID` FOREIGN KEY (`pizzaID`) REFERENCES `menu` (`pizzaID`)
@@ -25,7 +27,8 @@ CREATE TABLE `orderedPizza` (
 
 
 INSERT INTO `menu` VALUES
-	(1, 'Salami', './images/pizza-salami.jpg', 4.50),
-	(2, 'Margherita', './images/pizza-margherita.jpg', 4.00),
+	(1, 'Salami', './images/pizza-salami.jpg', 4.49),
+	(2, 'Margherita', './images/pizza-margherita.jpg', 3.99),
 	(3, 'Hawaii', './images/pizza-hawaii.jpg', 5.50),
-	(4, 'Peperoni', './images/pizza-peperoni.jpg', 4.50);
+	(4, 'Peperoni', './images/pizza-peperoni.jpg', 4.50),
+	(5, 'Käse', './images/pizza-käse.jpg', 4.99);
